9bf8dc382de2a1a75e271c18d9e51d5da7fc3856
Wilmer Ochoa <woalvarado@hotmail.com>
2018-04-04 15:29:23 -0500

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+<head>
+    <!-- META SECTION -->
+    <title>Joli Admin - Responsive Bootstrap Admin Template</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <!-- END META SECTION -->
+
+    <!-- CSS INCLUDE -->
+    <link rel="stylesheet" type="text/css" id="theme" href="{{asset('css/theme-default.css')}}"/>
+    <!-- EOF CSS INCLUDE -->
+</head>
+<body>
+
+<div class="login-container">
+
+    <div class="login-box animated fadeInDown">
+        <div class="login-logo"></div>
+        <div class="login-body">
+            <div class="login-title"><strong>Bienvenido</strong>, Iniciar sesión</div>
+            <form action="index.html" class="form-horizontal" method="post">
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" placeholder="Usuario"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input type="password" class="form-control" placeholder="Contraseña"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-6">
+                        <a href="#" class="btn btn-link btn-block">Olvidaste tu contraseña?</a>
+                    </div>
+                    <div class="col-md-6">
+                        <button class="btn btn-info btn-block">Log In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="login-footer">
+            <div class="pull-left">
+                &copy; 2017 Desarrollado por Ing. Wilmer ochoa
+            </div>
+            <div class="pull-right">
+                <!--<a href="#">About</a> |
+                <a href="#">Privacy</a> |-->
+                <a href="#">Contacto</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
